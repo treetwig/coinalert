493f8d37bad08a459d19ec6a8a8d0333d1942c0e
Chris <cosma.chris@gmail.com>
2016-10-26 19:03:47 -0400

    parse json

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var http = require('http');
-vra fs = require('fs');
+var fs = require('fs');
 
 var file = fs.createWriteStream("icn.json");
 var request = http.get("https://api.coinmarketcap.com/v1/ticker/iconomi/", function(response) {
 	response.pipe(file);
 });
+
+var icnObj = JSON.parse(fs.readFileSync('icn.json', 'utf8'));
+console.log(icnObj.percent_change_24h);
